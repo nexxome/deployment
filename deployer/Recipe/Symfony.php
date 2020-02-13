7f60f1e9a095234676f262371c263e1f06fcb39b
David <lochner@nexxo.me>
2020-02-13 11:02:53 +0100

    added translation tasks

--- deployer/Recipe/Symfony.php
+++ deployer/Recipe/Symfony.php
@@ -1,8 +1,11 @@
 <?php
 namespace nexxo\Deployer\Recipe;
 
+use Deployer\Exception\GracefulShutdownException;
+use function Deployer\input;
 use function Deployer\task;
 use function Deployer\run;
+use function Deployer\test;
 
 class Symfony {
     static function init(){
@@ -12,6 +15,23 @@ class Symfony {
         });
         task('cache:twig:clear', function () {
             run("rm -rf {{release_path}}/var/cache/prod/twig");
+        });
+
+        // translation tasks
+        task('deploy:translation:check_lock', function () {
+            $locked = test("[ -f {{deploy_path}}/current/translation.lock ]");
+
+            if ($locked) {
+                $stage = input()->hasArgument('stage') ? ' ' . input()->getArgument('stage') : '';
+
+                throw new GracefulShutdownException(
+                    "WARNING: Translation files have changed.\n" .
+                    sprintf('Check for changes and then execute "dep deploy:translation:unlock%s" to unlock.', $stage)
+                );
+            }
+        });
+        task('deploy:translation:unlock', function () {
+            run("rm -f {{deploy_path}}/current/translation.lock");
         });
     }
 }
